8f659198455125987ba861a17d79fd5beb65ec05
araft <alty29@gmail.com>
2021-11-18 18:38:53 -0500

    almost there

--- src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
+++ src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
@@ -28,33 +28,15 @@ public class JobData {
     private static ArrayList<PositionType> allPositionTypes = new ArrayList<>();
     private static ArrayList<CoreCompetency> allCoreCompetency = new ArrayList<>();
 
-    /**
-     * Fetch list of all job objects from loaded data,
-     * without duplicates, then return a copy.
-     */
-
     public static ArrayList<Job> findAll() {
 
-        // load data, if not already loaded
         loadData();
 
-        // Bonus mission; normal version returns allJobs
         return new ArrayList<>(allJobs);
     }
 
-    /**
-     * Returns the results of searching the Jobs data by field and search term.
-     *
-     * For example, searching for employer "Enterprise" will include results
-     * with "Enterprise Holdings, Inc".
-     *
-     * @param column Job field that should be searched.
-     * @param value Value of the field to search for.
-     * @return List of all jobs matching the criteria.
-     */
     public static ArrayList<Job> findByColumnAndValue(String column, String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<Job> jobs = new ArrayList<>();
@@ -95,15 +77,9 @@ public class JobData {
 
         return theValue;
     }
-    /**
-     * Search all Job fields for the given term.
-     *
-     * @param value The search term to look for.
-     * @return      List of all jobs with at least one field containing the value.
-     */
+
     public static ArrayList<Job> findByValue(String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<Job> jobs = new ArrayList<>();
@@ -136,19 +112,14 @@ public class JobData {
         return null;
     }
 
-    /**
-     * Read in data from a CSV file and store it in an ArrayList of Job objects.
-     */
     private static void loadData() {
 
-        // Only load data once
         if (isDataLoaded) {
             return;
         }
 
         try {
 
-            // Open the CSV file and set up pull out column header info and records
             Resource resource = new ClassPathResource(DATA_FILE);
             InputStream is = resource.getInputStream();
             Reader reader = new InputStreamReader(is);
@@ -159,7 +130,6 @@ public class JobData {
 
             allJobs = new ArrayList<>();
 
-            // Put the records into a more friendly format
             for (CSVRecord record : records) {
 
                 String aName = record.get(0);
@@ -197,7 +167,6 @@ public class JobData {
 
                 allJobs.add(newJob);
             }
-            // flag the data as loaded, so we don't do it twice
             isDataLoaded = true;
 
         } catch (IOException e) {
@@ -230,5 +199,9 @@ public class JobData {
         return allCoreCompetency;
     }
 
+    public static ArrayList<Job> getAll() {
+        loadData();
+        allJobs.sort(new NameSorter());
+        return allJobs;
+    }
 }
-
